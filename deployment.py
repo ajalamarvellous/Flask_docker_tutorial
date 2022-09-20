ebf194e1f617767584b19ee287fdd664e6e1626d
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-09-20 11:31:20 +0100

    Add a base deployment script to deploy the model as an api service This deployment script loads the pickled model (and preprocessing if any is used), create a flask app for the model to deploy as an api and deploy

---
+++ deployment.py
@@ -0,0 +1,38 @@
+import pickle
+import warnings
+warnings.simplefilter("ignore")
+
+from flask import Flask, request, jsonify
+
+
+def load_model(location):
+    """Loads the pickled model"""
+    with open(location, "rb") as file_in:
+        model = pickle.load(file_in)
+        # print("Model loaded sucessfully...")
+        return model
+
+app = Flask("California_model")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+    location = "./test_logit.pkl"
+    #vect = load_model("./vectorizer.bin")
+    model = load_model(location)
+    #data = vect.transform(data)
+    data = data["data"]
+    avg_price = model.predict(data)
+    if len(data) == 1:
+        result = {
+            "result": float(avg_price)
+            }
+    else:
+        result = {
+            "result": list(avg_price)
+        }
+    # print(f"The prediction of the data is {result}")
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=1200)
